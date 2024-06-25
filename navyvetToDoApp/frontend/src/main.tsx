56d339ecd898beac65ea120af34246e98662478f
Evan Washington <enavy04@gmail.com>
2024-06-25 04:37:39 +0000

    further built out the dashboard view

--- navyvetToDoApp/frontend/src/main.tsx
+++ navyvetToDoApp/frontend/src/main.tsx
@@ -8,7 +8,7 @@ import App from './App.tsx'
 import Layout from './layout/Layout.tsx';
 import DashboardLayout from './layout/DashboardLayout.tsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import './index.css'
+import './index.css'
 
 const router = createBrowserRouter([
   {
