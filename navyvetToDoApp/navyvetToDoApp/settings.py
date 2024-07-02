c1a2a27a9674402e0030b2d4e0a0f8f80e18926e
Evan Washington <enavy04@gmail.com>
2024-07-02 01:27:47 +0000

    utilized secret key stored in codespace settings

--- navyvetToDoApp/navyvetToDoApp/settings.py
+++ navyvetToDoApp/navyvetToDoApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-x_94e8(zdfk&5z-z8kjb!^gf^yo4s_l%spt^glzsiqv4fd1-(e"
+SECRET_KEY = os.getenv("TO_DO_APP_DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
